669f076ab68fe56b558224e4ce3167259ef20616
Jacob Madrid <jacob.madrid@upswell.jp>
2018-06-04 17:00:24 +0900

    Chart reformatted to double-Y axis. Also, X-axis is dates.

--- charts-test5/ChartViewController.swift
+++ charts-test5/ChartViewController.swift
@@ -51,16 +51,16 @@ class ChartViewController: UIViewController, GetChartData {
     }
     
     func populateChartData() { // any number of data sets can be specified in this case.
-        xAxis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24" ]
-        let yAxis1 = ["34", "100", "150", "10", "80", "92", "130", "160", "200", "110", "160", "180", "190", "90", "50", "120", "140", "190", "210", "180", "60", "180", "160", "330" ]
+        xAxis = ["29", "28", "27", "26", "25", "24", "23", "22", "21", "20", "19", "18", "17", "16", "15", "14", "13", "12", "11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0" ]
+        let yAxis1 = ["34", "100", "150", "10", "80", "92", "130", "160", "200", "110", "160", "180", "190", "90", "50", "120", "140", "190", "210", "180", "60", "180", "160", "330", "210", "180", "60", "180", "160", "330" ]
         let yAxis2 = ["12", "21", "15", "45", "39", "55", "19", "23", "11", "38", "29", "47",
-                      "12", "21", "15", "45", "39", "55", "19", "23", "11", "38", "29", "47"]
+                      "12", "21", "15", "45", "39", "55", "19", "23", "11", "38", "29", "47", "19", "23", "11", "38", "29", "47"]
         yAxis = [yAxis1, yAxis2]
         self.getChartData(with: xAxis, values: yAxis)
     }
     
     func populateChartDataTwo() { // any number of data sets can be specified in this case.
-        xAxis = ["1", "2", "3", "4", "5", "6", "7"]
+        xAxis = ["6", "5", "4", "3", "2", "1", "0"]
         let yAxis1 = ["34", "100", "150", "10", "80", "92", "13"]
         let yAxis2 = ["12", "21", "15", "45", "39", "55", "19"]
         yAxis = [yAxis1, yAxis2]
@@ -68,7 +68,7 @@ class ChartViewController: UIViewController, GetChartData {
     }
     
     func populateChartDataThree() { // any number of data sets can be specified in this case.
-        xAxis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+        xAxis = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"]
         let yAxis1 = ["80", "92", "130", "160", "200", "110", "160", "180", "190", "90"]
         let yAxis2 = ["90", "42", "70", "10", "90", "130", "120", "80", "64", "30"]
         yAxis = [yAxis1, yAxis2]
@@ -98,7 +98,7 @@ class ChartViewController: UIViewController, GetChartData {
     func lineChart() {
         let lineChart = LineChart(frame: CGRect(x: 0.0, y: self.view.frame.height * 0.15, width: self.view.frame.width, height: self.view.frame.height * 0.85))
         lineChart.delegate = self
-        let marker: BalloonMarker = BalloonMarker(color: UIColor.red, font: UIFont(name: "Helvetica", size: 12.0)!, textColor: UIColor.black, insets: UIEdgeInsetsMake(7.0, 7.0, 25.0, 7.0))
+        let marker: BalloonMarker = BalloonMarker(color: UIColor.blue, font: UIFont(name: "Helvetica", size: 12.0)!, textColor: UIColor.black, insets: UIEdgeInsetsMake(7.0, 7.0, 25.0, 7.0))
         marker.minimumSize = CGSize(width: 75.0, height: 35.0)
         lineChart.lineChartView.marker = marker
         self.view.addSubview(lineChart)
@@ -123,7 +123,6 @@ class ChartViewController: UIViewController, GetChartData {
         button3.setTitle("10d View", for: .normal)
         button3.addTarget(self, action: #selector(buttonClicked3(_ :)), for: .touchUpInside)
         self.view.addSubview(button3)
-        
     }
     
     func getChartData(with dataPoints: [String], values: [[String]]) {
@@ -135,12 +134,4 @@ class ChartViewController: UIViewController, GetChartData {
 }
 //The target function
 
-public class ChartFormatter: NSObject, IAxisValueFormatter {
-    var xAxis = [String]()
-    public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-        return xAxis[Int(value)]
-    }
-    public func setValues(values: [String]) {
-        self.xAxis = values
-    }
-}
+
--- charts-test5/LineChartView.swift
+++ charts-test5/LineChartView.swift
@@ -10,12 +10,26 @@ import Foundation
 import Charts
 import UIKit
 
-class LineChart: UIView {
+class LineChart: UIView, IAxisValueFormatter {
     let lineChartView = LineChartView()
     var lineDataEntry: [ChartDataEntry] = []
     
     var xAxis = [String]()
     var yAxis = [[String]]()
+    
+    public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        let date = NSCalendar.current.date(byAdding: .day, value: Int(Int(xAxis[Int(value)])! * -1), to: NSDate() as Date)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        dateFormatter.dateFormat = "MM月dd日"
+        //print() // 2001/01/0
+        return dateFormatter.string(from: date!)
+    }
+    public func setValues(values: [String]) {
+        self.xAxis = values
+    }
 
     var delegate: GetChartData! {
         didSet {
@@ -37,7 +51,7 @@ class LineChart: UIView {
         lineChartView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         lineChartView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         lineChartView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        lineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: ChartEasingOption.easeInSine)
+        //lineChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: ChartEasingOption.easeInSine)
         setLineChart(dataPoints: xAxis, values: yAxis)
     }
     
@@ -50,11 +64,12 @@ class LineChart: UIView {
             let dataPoint = ChartDataEntry(x: Double(i), y: Double(values[i])!)
             lineDataEntry.append(dataPoint)
         } */
-        let gradientColors = [UIColor.purple.cgColor, UIColor.clear.cgColor] as CFArray
-        let colorLocations: [CGFloat] = [1.0, 0.0]
-        let colors: [UIColor] = [UIColor.blue, UIColor.green]
-        guard let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) else { print("gradient error"); return }
+        //let gradientColors = [UIColor.purple.cgColor, UIColor.clear.cgColor] as CFArray
+        //let colorLocations: [CGFloat] = [1.0, 0.0]
+        let colors: [UIColor] = [UIColor.blue, UIColor.purple]
+        //guard let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) else { print("gradient error"); return }
         let labels: [String] = ["Weight", "Fat"]
+        let axes: [Charts.ChartYAxis.AxisDependency] = [Charts.YAxis.AxisDependency.left, Charts.YAxis.AxisDependency.right]
         let chartData = LineChartData()
         for i in 0..<values.count {
             lineDataEntry = []
@@ -65,31 +80,31 @@ class LineChart: UIView {
             let chartDataSet = LineChartDataSet(values: lineDataEntry, label: labels[i])
             chartData.addDataSet(chartDataSet)
             chartDataSet.colors = [colors[i]]
-            chartDataSet.lineWidth = 2.0
+            chartDataSet.lineWidth = 3.0
             chartDataSet.setCircleColor(colors[i])
             chartDataSet.circleHoleColor = colors[i]
             chartDataSet.circleRadius = 0.0
-            chartDataSet.fill = Fill.fillWithLinearGradient(gradient, angle: 90.0)
+            //chartDataSet.fill = Fill.fillWithLinearGradient(gradient, angle: 90.0)
             chartDataSet.drawFilledEnabled = true
+            chartDataSet.axisDependency = axes[i]
+            chartDataSet.mode = .cubicBezier
+            chartDataSet.cubicIntensity = 0.2
         }
         
         chartData.setDrawValues(false)
         
-        let formatter: ChartFormatter = ChartFormatter()
-        formatter.setValues(values: dataPoints)
-        let xaxis:XAxis = XAxis()
-        xaxis.valueFormatter = formatter
+        let xAxis = XAxis()
+        xAxis.valueFormatter = self
         lineChartView.xAxis.labelPosition = .bottom
         lineChartView.xAxis.drawGridLinesEnabled = false
-        lineChartView.xAxis.valueFormatter = xaxis.valueFormatter
+        lineChartView.xAxis.valueFormatter = xAxis.valueFormatter
+        lineChartView.xAxis.setLabelCount(5, force: true)
         lineChartView.chartDescription?.enabled = false
-        lineChartView.legend.enabled = true
-        lineChartView.rightAxis.enabled = false
+        lineChartView.legend.enabled = false
+        lineChartView.rightAxis.enabled = true
         lineChartView.leftAxis.drawGridLinesEnabled = false
         lineChartView.leftAxis.drawLabelsEnabled = true
         lineChartView.data = chartData
-        
-        
         
         
     }
