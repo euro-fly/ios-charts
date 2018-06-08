fc7869ce8da46efd07f4d4fc2b53712068b7d45e
Jacob Madrid <jacob.madrid@upswell.jp>
2018-06-08 15:15:12 +0900

    Layout fixes.

--- charts-test5/ObjCChartViewController.m
+++ charts-test5/ObjCChartViewController.m
@@ -16,33 +16,35 @@
 
 @protocol getChartData2
 
-@property NSArray* xAxis;
-@property NSArray* yAxis;
+@property NSMutableArray* xAxis;
+@property NSMutableArray* yAxis;
 
-- (void) getChartData: (NSArray*)dataPoints values:(NSArray*)values;
+- (void) getChartData: (NSMutableArray*)dataPoints values:(NSMutableArray*)values;
 @end
 
 @interface ObjCChartViewController :UIViewController <getChartData2>;
 @end
 
 @implementation ObjCChartViewController {
-    NSArray* xAxisMonthly;
-    NSArray* yAxisMonthly1;
-    NSArray* yAxisMonthly2;
-    NSArray* xAxisWeekly;
-    NSArray* yAxisWeekly1;
-    NSArray* yAxisWeekly2;
-    NSArray* xAxisYearly;
-    NSArray* yAxisYearly1;
-    NSArray* yAxisYearly2;
+    NSMutableArray* xAxisMonthly;
+    NSMutableArray* yAxisMonthly1;
+    NSMutableArray* yAxisMonthly2;
+    NSMutableArray* xAxisWeekly;
+    NSMutableArray* yAxisWeekly1;
+    NSMutableArray* yAxisWeekly2;
+    NSMutableArray* xAxisYearly;
+    NSMutableArray* yAxisYearly1;
+    NSMutableArray* yAxisYearly2;
     
     
     __weak IBOutlet LineChart *myLineChart;
     __weak IBOutlet UISegmentedControl *myButtonSet;
     __weak IBOutlet UILabel *weightLabel;
     __weak IBOutlet UILabel *fatLabel;
-    __weak IBOutlet UIToolbar *rotateButton;
-    __weak IBOutlet UIToolbar *updateButton;
+    __weak IBOutlet UIButton *rotateButton;
+    
+    __weak IBOutlet UIToolbar *toolbar;
+    __weak IBOutlet UIButton *updateButton;
 }
 - (IBAction)valueChanged:(id)sender {
     if (myButtonSet.selectedSegmentIndex == 0) {
@@ -55,26 +57,76 @@
     }
     [self refreshChart];
 }
+- (IBAction)rotate:(id)sender {
+    if (UIDeviceOrientationIsPortrait(UIDevice.currentDevice.orientation)) {
+        [UIDevice.currentDevice setValue:@(UIInterfaceOrientationLandscapeRight) forKey:@"orientation"];
+    }
+    else {
+        [UIDevice.currentDevice setValue:@(UIInterfaceOrientationPortrait) forKey:@"orientation"];
+    }
+}
+- (IBAction)update:(id)sender {
+    UIAlertController * alertController = [UIAlertController alertControllerWithTitle: @"Update"
+                                                                              message: @"Weight and Body Fat"
+                                                                       preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.placeholder = @"Weight";
+        textField.textColor = [UIColor blueColor];
+        textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+        textField.borderStyle = UITextBorderStyleRoundedRect;
+    }];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.placeholder = @"Bodyfat";
+        textField.textColor = [UIColor blueColor];
+        textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+        textField.borderStyle = UITextBorderStyleRoundedRect;
+    }];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        NSArray * textfields = alertController.textFields;
+        UITextField * bodyWeight = textfields[0];
+        UITextField * bodyFat = textfields[1];
+        NSLog(@"%@:%@",bodyWeight.text,bodyFat.text);
+        NSPredicate *predicate;
+        predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES '^[-+]?[0-9]*\.?[0-9]+$'"];
+        BOOL result1 = [predicate evaluateWithObject:bodyWeight.text];
+        BOOL result2 = [predicate evaluateWithObject:bodyFat.text];
+        if (result1) {
+            self.yAxis[0][self.xAxis.count - 1] = bodyWeight.text;
+        }
+        if (result2) {
+            self.yAxis[1][self.xAxis.count - 1] = bodyFat.text;
+        }
+        if (!(!result1 && !result2)) {
+            [self getChartData:self.xAxis values:self.yAxis];
+            [self refreshChart];
+        }                                                                                       
+    }]];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    xAxisMonthly = [[NSArray alloc] initWithObjects:@"29", @"28", @"27", @"26", @"25", @"24", @"23", @"22", @"21", @"20", @"19", @"18", @"17", @"16", @"15", @"14", @"13", @"12", @"11", @"10", @"9", @"8", @"7", @"6", @"5", @"4", @"3", @"2", @"1", @"0", nil];
-    yAxisMonthly1 = [[NSArray alloc] initWithObjects: @"56.2", @"56.4", @"54.2", @"56.7", @"54.5", @"53.1", @"59.4", @"51", @"58", @"56", @"50", @"58", @"55", @"56", @"50", @"58", @"59", @"51", @"58", @"51", @"53", @"52", @"56", @"56", @"52", @"52", @"59", @"53", @"55", @"52", nil];
-    yAxisMonthly2 = [[NSArray alloc] initWithObjects: @"23", @"26", @"29", @"28", @"25", @"28", @"21", @"24", @"22", @"26", @"22", @"23", @"21", @"23", @"22", @"22", @"29", @"22", @"26", @"22", @"22", @"23", @"20", @"23", @"28", @"25", @"24", @"24", @"27", @"24", nil];
+    xAxisMonthly = [[NSMutableArray alloc] initWithObjects:@"29", @"28", @"27", @"26", @"25", @"24", @"23", @"22", @"21", @"20", @"19", @"18", @"17", @"16", @"15", @"14", @"13", @"12", @"11", @"10", @"9", @"8", @"7", @"6", @"5", @"4", @"3", @"2", @"1", @"0", nil];
+    yAxisMonthly1 = [[NSMutableArray alloc] initWithObjects: @"56.2", @"56.4", @"54.2", @"56.7", @"54.5", @"53.1", @"59.4", @"51", @"58", @"56", @"50", @"58", @"55", @"56", @"50", @"58", @"59", @"51", @"58", @"51", @"53", @"52", @"56", @"56", @"52", @"52", @"59", @"53", @"55", @"52", nil];
+    yAxisMonthly2 = [[NSMutableArray alloc] initWithObjects: @"23", @"26", @"29", @"28", @"25", @"28", @"21", @"24", @"22", @"26", @"22", @"23", @"21", @"23", @"22", @"22", @"29", @"22", @"26", @"22", @"22", @"23", @"20", @"23", @"28", @"25", @"24", @"24", @"27", @"24", nil];
     
-    xAxisWeekly = [[NSArray alloc] initWithObjects:@"6", @"5", @"4", @"3", @"2", @"1", @"0", nil];
-    yAxisWeekly1 = [[NSArray alloc] initWithObjects:@"54", @"52", @"52", @"54", @"57", @"57", @"50", nil];
-    yAxisWeekly2 = [[NSArray alloc] initWithObjects:@"23", @"24", @"21", @"21", @"25", @"29", @"29", nil];
+    xAxisWeekly = [[NSMutableArray alloc] initWithObjects:@"6", @"5", @"4", @"3", @"2", @"1", @"0", nil];
+    yAxisWeekly1 = [[NSMutableArray alloc] initWithObjects:@"54", @"52", @"52", @"54", @"57", @"57", @"50", nil];
+    yAxisWeekly2 = [[NSMutableArray alloc] initWithObjects:@"23", @"24", @"21", @"21", @"25", @"29", @"29", nil];
     
-    xAxisYearly = [[NSArray alloc] initWithObjects:@"11", @"10", @"9", @"8", @"7", @"6", @"5", @"4", @"3", @"2", @"1", @"0", nil];
-    yAxisYearly1 = [[NSArray alloc] initWithObjects:@"50", @"59", @"52", @"50", @"50", @"56", @"57", @"60.7", @"58", @"51", @"51", @"51", nil];
-    yAxisYearly2 = [[NSArray alloc] initWithObjects:@"26", @"25", @"26", @"29", @"23", @"24", @"25", @"26", @"24", @"24", @"30", @"28", nil];
+    xAxisYearly = [[NSMutableArray alloc] initWithObjects:@"11", @"10", @"9", @"8", @"7", @"6", @"5", @"4", @"3", @"2", @"1", @"0", nil];
+    yAxisYearly1 = [[NSMutableArray alloc] initWithObjects:@"50", @"59", @"52", @"50", @"50", @"56", @"57", @"60.7", @"58", @"51", @"51", @"51", nil];
+    yAxisYearly2 = [[NSMutableArray alloc] initWithObjects:@"26", @"25", @"26", @"29", @"23", @"24", @"25", @"26", @"24", @"24", @"30", @"28", nil];
     
     // Do any additional setup after loading the view, typically from a nib.
     
     //[self.view addSubview:_myButton];
     
     [self.view addSubview:myLineChart];
+    [self.view addSubview:toolbar];
+    [toolbar addSubview:rotateButton];
+    [toolbar addSubview:updateButton];
     [self populateChartDataThree];
     [self lineChart];
 }
@@ -86,19 +138,19 @@
 
 - (void) populateChartData {
     xAxis = xAxisMonthly;
-    yAxis = [[NSArray alloc] initWithObjects:yAxisMonthly1, yAxisMonthly2, nil];
+    yAxis = [[NSMutableArray alloc] initWithObjects:yAxisMonthly1, yAxisMonthly2, nil];
     [self getChartData:xAxis values:yAxis];
 }
 
 - (void) populateChartDataTwo {
     xAxis = xAxisWeekly;
-    yAxis = [[NSArray alloc] initWithObjects:yAxisWeekly1, yAxisWeekly2, nil];
+    yAxis = [[NSMutableArray alloc] initWithObjects:yAxisWeekly1, yAxisWeekly2, nil];
     [self getChartData:xAxis values:yAxis];
 }
 
 - (void) populateChartDataThree {
     xAxis = xAxisYearly;
-    yAxis = [[NSArray alloc] initWithObjects:yAxisYearly1, yAxisYearly2, nil];
+    yAxis = [[NSMutableArray alloc] initWithObjects:yAxisYearly1, yAxisYearly2, nil];
     [self getChartData:xAxis values:yAxis];
 }
 
@@ -112,15 +164,10 @@
     
     
 }
-- (IBAction)rotate:(id)sender {
-}
 
 - (void) updateLabel: (NSString*)weight fat:(NSString*)fat {
     weightLabel.text = weight;
     fatLabel.text = fat;
-}
-
-- (IBAction)update:(id)sender {
 }
 
 - (void)didReceiveMemoryWarning {
@@ -129,7 +176,7 @@
 }
 
 
-- (void)getChartData: (NSArray*)dataPoints values:(NSArray*)values {
+- (void)getChartData: (NSMutableArray*)dataPoints values:(NSMutableArray*)values {
     self.xAxis = dataPoints;
     self.yAxis = values;
 }
